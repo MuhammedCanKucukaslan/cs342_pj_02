75b6d74ee26694300918b9e09bf8edba53d1cef7
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2022-03-17 16:15:23 +0300

    Implement the queue, start implementing the pgen and psched

--- shared.c
+++ shared.c
@@ -1,24 +1,48 @@
-enum state {RUNNING, READY, WAITING};
-enum algorithm {FCFS, SJF, RR};
-enum burst_distribution {UNI, EXP, FIX};
+enum state
+{
+    RUNNING,
+    READY,
+    WAITING
+};
+enum algorithm
+{
+    FCFS,
+    SJF,
+    RR
+};
+enum burst_distribution
+{
+    UNI,
+    EXP,
+    FIX
+};
+
+enum task_type
+{
+    CPU,
+    TERMINATE,
+    IO1,
+    IO2
+};
 /*
 #define FCFS "FCFS"
 #define SJF "SJF"
 #define RR  "RR"
 */
-struct PCB {
-    //pid (a positive integer)
-    //thread id (of type pthread_t), 
-    //state, 
-    //next CPU burst length (ms), 
-    //remaining CPU burst length to execute (ms) (for RR scheduling),
-    //number of CPU bursts executed so far, 
-    //time spent in ready list so far (ms), 
-    //number of times I/O is done with device1,
-    //number of times I/O is done with device2,
-    //start time (ms), i.e., arrival time, 
-    //finish time (ms),
-    //total execution time in CPU so far
+struct PCB
+{
+    // pid (a positive integer)
+    // thread id (of type pthread_t),
+    // state,
+    // next CPU burst length (ms),
+    // remaining CPU burst length to execute (ms) (for RR scheduling),
+    // number of CPU bursts executed so far,
+    // time spent in ready list so far (ms),
+    // number of times I/O is done with device1,
+    // number of times I/O is done with device2,
+    // start time (ms), i.e., arrival time,
+    // finish time (ms),
+    // total execution time in CPU so far
     int pid;
     pthread_t tid;
     enum state state;
@@ -31,5 +55,39 @@ struct PCB {
     int start_time;
     int finish_time;
     int total_execution_time;
-
 };
+
+
+
+struct pcb_node
+{
+    struct PCB pcb;
+    struct pcb_node *next;
+};
+
+struct pcb_queue
+{
+    struct pcb_node *head;
+    struct pcb_node *tail;
+    int count;
+};
+/* DEPRECATED
+void pcb_to_pcbptr(struct PCB pcb, struct PCB *pcbptr)
+{
+    // copy the pcb data to the pcbptr
+    pcbptr->pid = pcb.pid;
+    pcbptr->tid = pcb.tid;
+    pcbptr->state = pcb.state;
+    pcbptr->next_burst_length = pcb.next_burst_length;
+    pcbptr->remaining_burst_length = pcb.remaining_burst_length;
+    pcbptr->num_bursts = pcb.num_bursts;
+    pcbptr->time_in_ready_list = pcb.time_in_ready_list;
+    pcbptr->IO_device1 = pcb.IO_device1;
+    pcbptr->IO_device2 = pcb.IO_device2;
+    pcbptr->start_time = pcb.start_time;
+    pcbptr->finish_time = pcb.finish_time;
+    pcbptr->total_execution_time = pcb.total_execution_time;
+
+
+}
+*/
--- systemsim.c
+++ systemsim.c
@@ -8,6 +8,8 @@
 #include <pthread.h>
 
 #include "shared.c"
+
+// declare arguments
 enum algorithm alg;
 int Q;
 int T1;
@@ -24,15 +26,27 @@ int MAXP;
 int ALLP;
 int outmode;
 
+// declare global variables
+struct pcb_queue ready_queue = {NULL, NULL, 0};
+int toBeRun_pid = -1;
 
-void process();
+pthread_mutex_t pcb_queue_mutex;
+pthread_cond_t cpu_cond_var;
+
+// declare methods!
+void *process(void *arg);
+void *p_sched(void *arg);
+int pcb_queue_dequeue();
+void mutex_queue_add(struct PCB pcb);
+int mutex_queue_rm();
+void pcb_queue_insert(struct PCB pcb);
 
 int main(int argc, char **argv)
 {
     // READ THE arguments
     if (argc != 16)
     {
-        printf("Usage: %s <message queue name> arg. count %s\n", argv[0], argc);
+        printf("Usage: %s <message queue name> arg. count %d\n", argv[0], argc);
         exit(1);
     }
     // <ALG> <Q> <T1> <T2> <burst-dist> <burstlen> <min-burst> <max-burst> <p0> <p1> <p2> <pg> <MAXP> <ALLP> <OUTMODE>
@@ -125,10 +139,12 @@ void *p_gen(void *arg)
     for (int i = 0; i < 10; i++)
     {
         pthread_t tid;
-        pthread_create(&tid, NULL, process, NULL);
-        // TODO add tid to rdy queue
+        // TODO add tid to ready queue
         maxp_count++;
         allp_count++;
+
+        struct PCB the_pcb = {.pid = allp_count, .tid = 0, .state = READY, .next_burst_length = 0, .remaining_burst_length = 0, .num_bursts = 0, .time_in_ready_list = 0, .IO_device1 = 0, .IO_device2 = 0, .start_time = 0, .finish_time = 0, .total_execution_time = 0};
+        pthread_create(&tid, NULL, process, (void *)the_pcb);
     }
 
     while (allp_count <= ALLP)
@@ -136,7 +152,7 @@ void *p_gen(void *arg)
         pthread_t tid;
         // sleep for 5ms
         usleep(5000);
-        float rn = ((float)rand()) / RAND_MAX;
+        float rn = frandom();
         if (rn > pg)
         {
             pthread_create(&tid, NULL, process, NULL);
@@ -151,66 +167,171 @@ void *p_gen(void *arg)
 
 void *p_sched(void *arg)
 {
-    // create ready queue
-    int maxp_count = 0;
-    int allp_count = 0;
-    for (int i = 0; i < 10; i++)
+    toBeRun_pid = mutex_queue_rm();
+    if (toBeRun_pid == -1)
     {
-        pthread_t tid;
-        pthread_create(&tid, NULL, process, NULL);
-        // TODO add tid to rdy queue
-        maxp_count++;
-        allp_count++;
+        // empty queue what to do?
+    }
+    else
+    {
+        // TODO run process
+        pthread_cond_broadcast(&cpu_cond_var);
     }
 
-    while (allp_count <= ALLP)
-    {
-        pthread_t tid;
-        // sleep for 5ms
-        usleep(5000);
-        float rn = ((float)rand()) / RAND_MAX;
-        if (rn > pg)
-        {
-            pthread_create(&tid, NULL, process, NULL);
-            // TODO add tid to rdy queue
-            maxp_count++;
-            allp_count++;
-        }
-    }
     // TODO wait for all processes to finish
     pthread_exit(NULL);
 }
 
-void process() {
+void *process(void *arg)
+{
     // todo implement process
-     while (!terminated)
+    // pthread_self()
+    enum task_type task = CPU;
+    float rn;
+    // cast *arg to PCB
+    struct PCB my_pcb = (struct PCB)arg;
+    // update pcb datas
+    my_pcb.tid = pthread_self();
+
+    while (task != TERMINATE)
     {
-            //  if (state == Ready)
-            buf_add(buf, index, i);
-            if (cpu)
+        mutex_queue_add(my_pcb);
+
+        while (toBeRun_pid != my_pcb.pid)
+        {
+            // todo what mutex should be here?
+            pthread_cond_wait(&cpu_cond_var, &pcb_queue_mutex);
+        }
+        
+    //        pthread_cond_wait(&cpu_cond_var, &mut);
+        //  if (state == Ready)
+        buf_add(buf, index, i);
+
+        if (cpu)
+        {
+            while (notmyturn)
             {
-                    while (notmyturn)
-                    {
-                            pthread_cond_wait(cpu);
-                    }
-                    usleep(PCB.sleep_time);
-                    // todo update PCB
-                    determine next
+                pthread_cond_wait(cpu);
             }
-            else if(dev1) {
-                    while (notmyturn)
-                    {
-                            pthread_cond_wait(dev1);
-                    }
-                    // todo use dev1
+            usleep(PCB.sleep_time);
+            // todo update PCB
+            determine next
+        }
+        else if (dev1)
+        {
+            while (notmyturn)
+            {
+                pthread_cond_wait(dev1);
             }
-            else if(dev2) {
-                    while (notmyturn)
-                    {
-                            pthread_cond_wait(dev2);
-                    }
-                    // todo use dev2
+            // todo use dev1
+        }
+        else if (dev2)
+        {
+            while (notmyturn)
+            {
+                pthread_cond_wait(dev2);
             }
+            // todo use dev2
+        }
+        rn = frandom();
+        if (rn < p0)
+        {
+            task = TERMINATE;
+        }
+        else if (rn < p0 + p1)
+        {
+            task = IO1;
+        }
+        else if (rn <= 1)
+        {
+            task = IO2;
+        }
     }
+
     pthread_exit(NULL);
+}
+
+void mutex_queue_add(struct PCB pcb)
+{
+    // lock mutex
+    pthread_mutex_lock(&pcb_queue_mutex);
+    // add pcb to queue
+    pcb_queue_insert(pcb);
+    // unlock mutex
+    pthread_mutex_unlock(&pcb_queue_mutex);
+};
+
+int mutex_queue_rm()
+{
+    // lock mutex
+    pthread_mutex_lock(&pcb_queue_mutex);
+    // select next pcb
+    int pid = pcb_queue_dequeue();
+    // unlock mutex
+    pthread_mutex_unlock(&pcb_queue_mutex);
+    return pid;
+};
+
+void pcb_queue_insert(struct PCB pcb)
+{
+    enum algorithm ins_alg = alg;
+    struct pcb_node *node = malloc(sizeof(struct pcb_node));
+    node->pcb = pcb;
+    node->next = NULL;
+
+    if (ready_queue.count == 0)
+    {
+        ready_queue.head = node;
+        ready_queue.tail = node;
+    }
+    else if (ins_alg == SJF)
+    {
+        struct pcb_node *curr = ready_queue.head;
+        struct pcb_node *prev = NULL;
+        while (curr != NULL && curr->pcb.next_burst_length < node->pcb.next_burst_length)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        if (prev == NULL)
+        {
+            node->next = ready_queue.head;
+            ready_queue.head = node;
+        }
+        else
+        {
+            prev->next = node;
+            node->next = curr;
+        }
+        if (curr == NULL)
+            ready_queue.tail = node;
+    }
+    else
+    {
+        ready_queue.tail->next = node;
+        ready_queue.tail = node;
+    }
+    ready_queue.count++;
+}
+
+int pcb_queue_dequeue()
+{
+    if (ready_queue.count == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        struct pcb_node *node = ready_queue.head;
+        ready_queue.head = node->next;
+        ready_queue.count--;
+        if (ready_queue.count == 0)
+            ready_queue.tail = NULL;
+        return node->pcb.pid;
+    }
+}
+
+float frandom()
+{
+    return (float)rand() / RAND_MAX;
 }
